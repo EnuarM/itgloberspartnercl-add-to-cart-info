af19b8ddc7604efba00c70d0b9be4244021a5465
EnuarM <99690878+EnuarM@users.noreply.github.com>
2022-12-09 17:26:01 -0500

    upgrade

---
+++ react/components/ButtonGroup.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ButtonGroup = () => {
+  return (
+    <>
+      <div>
+        <a href="/">Checkout</a>
+      </div>
+      <div>
+        <button>Continua comprando</button>
+        <a href="/">Ver carrito</a>
+      </div>
+    </>
+  )
+}
+
+export default ButtonGroup
+
