98a64bcf8c837814c9622b510a227009d1f8c4d6
EnuarM <99690878+EnuarM@users.noreply.github.com>
2022-12-11 14:29:01 -0500

    component done without css

--- react/components/AddToCartInfo..tsx
+++ react/components/AddToCartInfo..tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useProduct } from 'vtex.product-context'
 import { useOrderForm } from 'vtex.order-manager/OrderForm'
 import ButtonGroup from './ButtonGroup'
+// import Totalizers from './Totalizers'
+
 
 const AddToCartInfo = () => {
   const productInfo = useProduct()
@@ -10,11 +12,30 @@ const AddToCartInfo = () => {
     totalizers
   } } = useOrderForm()
   console.log("Este producto tiene esta info: ", productInfo)
-  console.log("Esta es mi informacion de la orden: ", items, totalizers)
+  console.log("Estos son mis totales: ", totalizers)
+
   return (
     <>
-      {/* <ProductGroup />
-      <Totalizers /> */}
+      {items.map((item: any, index: number) => {
+        console.log(item)
+        return (
+          <div key={index}>
+            <div>
+              <img src={item.imageUrls.at1x} />
+            </div>
+            <div>
+              <p>{item.name}</p>
+              <p>{item.id}</p>
+              <p>${item.price / 100}</p>
+              <p>Cantidad:{item.quantity}</p>
+            </div>
+          </div>)
+      }
+      )}
+      <div>
+        <p>Tenemos {items.length} items en tu compra</p>
+        <p>Total: ${totalizers[0]?.value / 100} </p>
+      </div>
       <ButtonGroup />
     </>
   )
---
+++ react/components/ProductGroup.tsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+// type Props = {
+//   products: any[]
+// }
+// const ProductGroup = ({products}:Props) => {
+//   console.log("Mis productos de product group son: ", products)
+//   return (
+//     <div>
+//       Mi listado de productos
+//     </div>
+//   )
+// }
+
+// export default ProductGroup
